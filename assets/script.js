1efc83cc398e129ec8abf97a601e5db995e5019b
caditta <caditta@gmail.com>
2024-06-26 20:48:58 -0500

    Transicion de paginas con JS y creacion del archivo script

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+function rdashboard(){
+    $(document).ready(function () {
+        $("#rdashboard").fadeIn(1000);
+    });
+}
+function rplanes(){
+    $(document).ready(function () {
+        $("#rplanes").fadeIn(1000);
+    });
+}
+function rportafolio(){
+    $(document).ready(function () {
+        $("#rportafolio").fadeIn(1000);
+    });
+}
+function rsomos(){
+    $(document).ready(function () {
+        $("#rsomos").fadeIn(1000);
+    });
+}
+function rindex(){
+    $(document).ready(function () {
+        $("#rindex").fadeIn(2000);
+    });
+}
